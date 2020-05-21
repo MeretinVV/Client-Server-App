f7941d89a847077114637058d32aa781a8869aa0
MeretinVV <46193772+MeretinVV@users.noreply.github.com>
2020-05-21 12:07:46 +0300

    Add files via upload

---
+++ TCP Connections/Tcp.Server/EnteringPointServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TCPConnections.Library.Server;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace TCPConnections.TcpServer
+{
+    class EnteringPointServer
+    {
+        static void Main(string[] args)
+        {
+           try
+            {
+                Server server = new Server();
+                List<Task> listeners = new List<Task>();
+
+                listeners.Add(Task.Run(() => server.TurnOnFileListener()));
+                listeners.Add(Task.Run(() => server.TurnOnMessageListener()));
+
+                Task.WaitAll(listeners.ToArray());
+
+                //turn off by command: server.Stop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
